f8a08a6ff16754913b13372d3056804274355cec
Imre <imre.tatai@gmail.com>
2024-09-05 00:05:13 +0200

    Changed Config

--- src/main/java/co/grtk/virtualthreaddemo/AppConfig.java
+++ src/main/java/co/grtk/virtualthreaddemo/AppConfig.java
@@ -1,13 +1,8 @@
 package co.grtk.virtualthreaddemo;
 
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.task.TaskDecorator;
 import org.springframework.web.client.RestClient;
-
-import java.util.ArrayList;
-import java.util.concurrent.ConcurrentSkipListSet;
 
 @Configuration
 public class AppConfig {
