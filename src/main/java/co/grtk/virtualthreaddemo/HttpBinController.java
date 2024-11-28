c468a18b138e81574017797bcedacd00f8ece4bf
Imre <imre.tatai@gmail.com>
2024-11-28 21:03:48 +0100

    partialReactiveBlocks and reorg

--- src/main/java/co/grtk/virtualthreaddemo/HttpBinController.java
+++ src/main/java/co/grtk/virtualthreaddemo/HttpBinController.java
@@ -2,15 +2,20 @@ package co.grtk.virtualthreaddemo;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClient;
 
+import java.util.ArrayList;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -20,33 +25,60 @@ public class HttpBinController {
     final RestClient restClient;
     long sum;
     AtomicInteger counter = new AtomicInteger();
+    AtomicInteger threadCounter = new AtomicInteger();
 
     @GetMapping("/block/{seconds}")
-    public String delay(@PathVariable int seconds) {
+    public String block(@PathVariable int seconds) {
+        waitForRest(counter.incrementAndGet(), 1, seconds);
+        return Thread.currentThread().toString();
+    }
+
+    private String waitForRest(int requestNr, int threadNr, int seconds) {
         long start = System.currentTimeMillis();
         ResponseEntity<Void> result = restClient.get()
                 .uri("/delay/" + seconds)
                 .retrieve()
                 .toBodilessEntity();
         long elapsed = System.currentTimeMillis() - start;
-        sum += elapsed;
-        log.info("# {} {} on {} blocked {} sec elapsed {} ms sum {} ms", counter.incrementAndGet(), result.getStatusCode(), Thread.currentThread(), seconds, elapsed, sum);
-        return Thread.currentThread().toString();
+        return "REST Call finished requestNr:" + requestNr + " threadNr:" + threadNr + " nrOfFinishedThreads: " + threadCounter.incrementAndGet() + " result:" + result.getStatusCode() + " elapsed: " + elapsed;
     }
 
-    @GetMapping("/block1/{seconds}")
-    public String delay1(@PathVariable int seconds) {
+    private String waitForThread(int requestNr, int threadNr, int seconds) {
         long start = System.currentTimeMillis();
+        try {
+            TimeUnit.SECONDS.sleep(seconds);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        long elapsed = System.currentTimeMillis() - start;
+        return "Thread sleep finished requestNr:" + requestNr + " threadNr:" + threadNr + " nrOfFinishedThreads: " + threadCounter.incrementAndGet() + " elapsed: " + elapsed;
+    }
 
-        CompletableFuture<HttpStatusCode> future = CompletableFuture.supplyAsync(restClient.get()
-                .uri("/delay/" + seconds)
-                .retrieve()::toBodilessEntity).thenApply(ResponseEntity::getStatusCode);
+    @GetMapping("/partialReactiveBlocks/{seconds}/threads/{nrOfThreads}")
+    public String partialReactiveBlocks(@PathVariable int seconds, @PathVariable int nrOfThreads) throws ExecutionException, InterruptedException, TimeoutException {
 
-        HttpStatusCode result = future.join();
+        long start = System.currentTimeMillis();
+        final int requestCounter = counter.incrementAndGet();
+        ArrayList<CompletableFuture<String>> futures = new ArrayList<>();
+
+        for (int i = 1; i <= nrOfThreads; i++) {
+            final int threadNr = i;
+            CompletableFuture<String> future
+                    = CompletableFuture.supplyAsync(() -> waitForRest(requestCounter, threadNr, seconds));
+           futures.add(future);
+        }
+
+        CompletableFuture<Void> combinedFuture = CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]));
+        combinedFuture.get(30L, TimeUnit.SECONDS);
+
+        String combined = Stream.of(futures.toArray(new CompletableFuture[0]))
+                .map(CompletableFuture::join)
+                .map(Object::toString)
+                .collect(Collectors.joining("\n"));
 
         long elapsed = System.currentTimeMillis() - start;
         sum += elapsed;
-        log.info("# {} {} on {} blocked {} sec elapsed {} ms sum {} ms", counter.incrementAndGet(), result, Thread.currentThread(), seconds, elapsed, sum);
+        log.info("\n{}\n# {} nr of partialReactiveBlocks threads {} on {} blocked {} sec elapsed {} ms sum {} ms", combined, requestCounter, nrOfThreads, Thread.currentThread(), seconds, elapsed, sum);
         return Thread.currentThread().toString();
     }
 
